a544ec3629b75532e0eb2739296fe42b57e840c1
Kristo Papa <54607508+kristopapa@users.noreply.github.com>
2021-01-14 17:53:04 -0500

    Create .sql
    *THIS IS A FILE FOR TESTING PURPOSES ONLY AND NOT MY CODE* *I DID NOT WRITE THIS QUERY*
    OP: https://www.mysqltutorial.org/mysql-join/

---
+++ mysql/.sql
@@ -0,0 +1,11 @@
+CREATE TABLE members (
+    member_id INT AUTO_INCREMENT,
+    name VARCHAR(100),
+    PRIMARY KEY (member_id)
+);
+
+CREATE TABLE committees (
+    committee_id INT AUTO_INCREMENT,
+    name VARCHAR(100),
+    PRIMARY KEY (committee_id)
+);
